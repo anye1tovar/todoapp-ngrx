5c06658d3bf8a7350e39d4900c0f129d747f28a4
anye1tovar <natovar@uniboyaca.edu.co>
2023-06-07 12:10:10 -0500

    :sparkles: Config project components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { StoreModule } from '@ngrx/store';
+import { todoReducer } from './todos/todo.reducer';
+
 import { AppComponent } from './app.component';
+import { TodoModule } from './todos/todo.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, FooterComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TodoModule,
+    StoreModule.forRoot({ count: todoReducer }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
